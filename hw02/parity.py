979ff0627dccbd185e96642cd519c92e3a4f53e4
Charles Henneberger <cihen@bu.edu>
2020-09-18 17:14:52 -0400

    Finished z3py script.

--- hw02/parity.py
+++ hw02/parity.py
@@ -2,3 +2,38 @@ from z3 import *
 
 p1, p2, p3, p4 = Bools('p1 p2 p3 p4')
 
+phi = And( \
+	Or(Not(p1), Not(p2), Not(p3), p4), \
+	Or(p1, p2, p3, Not(p4)), \
+	Or(Not(p1), Not(p2), p3, Not(p4)), \
+	Or(p1, p2, Not(p3), p4), \
+	Or(Not(p1), p2, Not(p3), Not(p4)), \
+	Or(p1, Not(p2), p3, p4), \
+	Or(p1, Not(p2), Not(p3), Not(p4)), \
+	Or(Not(p1), p2, p3, p4) \
+	)
+
+psi = Or( \
+	And(p1, p2, p3, p4), \
+	And(Not(p1), Not(p2), Not(p3), Not(p4)), \
+	And(p1, p2, Not(p3), Not(p4)), \
+	And(Not(p1), Not(p2), p3, p4), \
+	And(p1, Not(p2), p3, Not(p4)), \
+	And(Not(p1), p2, Not(p3), p4), \
+	And(p1, Not(p2), Not(p3), p4), \
+	And(Not(p1), p2, p3, Not(p4)) \
+	)
+
+theta = ((((p1 == True) == (p4 == False)) == ((p1 == p2) == (p1 == p3))) == False)
+
+a = Solver()
+a.add(Not(phi == psi))
+print a.check()
+
+b = Solver()
+b.add(Not(phi == theta))
+print b.check()
+
+c = Solver()
+c.add(Not(psi == theta))
+print c.check()
